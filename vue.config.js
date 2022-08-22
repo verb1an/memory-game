ef272e96a0c2adaa1f441ea860fed92d1d62bacd
verb1an <tabyshkinalex@gmail.com>
2022-08-22 16:21:49 +0700

    v0.1.1

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,10 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/memory-game/dist/'
-    : '/'
+    transpileDependencies: true
 })
+
+module.exports = {
+    publicPath: process.env.NODE_ENV === 'production'
+        ? '/memory-game/dist/'
+        : '/'
+}
